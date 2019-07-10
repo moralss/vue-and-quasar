d0a1087f73be5233c94fd1b54d5a535370b03b27
moralss <jeramoral@gmail.com>
2019-07-10 10:26:08 +0200

    created a new vue project

---
+++ recipe-box/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Recipes />
+  </div>
+</template>
+
+<script>
+import Recipes from './components/Recipes.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Recipes
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ recipe-box/src/components/Recipes.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <h1>List of Recipes</h1>
+  <li v-for="(recipe , index) in recipes" :key="index">
+    <h1>{{recipe.recipeName}}</h1>
+  </li>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "Recipes",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      recipes: [
+        {
+          recipeName: 'Seafood',
+          ingredients: ['milk', 'fish']
+        },
+        {
+          recipeName: 'pop',
+          ingredients: ['sun flower', 'water']
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
